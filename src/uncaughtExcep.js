f7aed1bca14f6b0038ada25b0cc587241ef3e279
678435021 <amongusthem69@gmail.com>
2022-12-11 01:24:30 +0100

    I forgor

--- src/uncaughtExcep.js
+++ src/uncaughtExcep.js
@@ -2,14 +2,13 @@ const { error, warn } = require('../lib/log');
 const PACKAGE = require('../package.json');
 
 function set (debug) {
-	let onUncaughtException;
+	process.on('uncaughtException', (err) => {
+		if (typeof err !== 'object') {
+			error(`An unexpected error occurred.\n${err}`);
+			return;
+		}
 
-	if (debug === false) {
-		onUncaughtException = (err) => {
-			if (typeof err !== 'object') {
-				error(`An unexpected error occurred.\n${err}`);
-				return;
-			}
+		if (debug === false) {
 			const stack = err.stack?.split('\n');
 			let relevant = '';
 			if (stack[1]) relevant = `\n${stack[1]}`;
@@ -18,13 +17,10 @@ function set (debug) {
 			error(`An unexpected error occurred.\n${err.message}${relevant}`);
 			warn(`Please open a bug report on github: ${PACKAGE.bugs.url}`);
 			process.exit(1);
-		};
-	} else {
-		onUncaughtException = (err) => {
+		} else {
 			process.stdout.write(err.stack);
-		};
-	}
-	process.on('uncaughtException', onUncaughtException);
+		}
+	});
 }
 
 module.exports = set;
