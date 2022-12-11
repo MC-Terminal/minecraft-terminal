8f71a083b5636082397b7e05d900240ca759af1c
678435021 <amongusthem69@gmail.com>
2022-12-11 21:24:30 +0100

    Fix promptCred

--- src/promptCred.js
+++ src/promptCred.js
@@ -6,25 +6,25 @@ module.exports = async (settings, chat) => {
 	prompt.setInterface(chat);
 
 	// Check if auth is defined or null
-	if (!settings.bot.cred.auth) {
+	if (!settings.bot.cred.auth && settings.bot.cred.auth !== null) {
 		settings.bot.cred.auth = await prompt('Auth :');
-		if (settings.bot.cred.auth.toLowerCase() === 'mojang') {
+		if (settings.bot.cred.auth?.toLowerCase() === 'mojang') {
 			warn('Mojang auth servers no longer accept mojang accounts to login.\nThat means you can no longer use mojang accounts', 2);
 			process.exit(1);
 		}
 	}
 
 	// Check if username is defined or null
-	if (!settings.bot.cred.username) {
+	if (!settings.bot.cred.username && settings.bot.cred.username !== null) {
 		settings.bot.cred.username = await prompt('Login :');
-		if (settings.bot.cred.auth.toLowerCase() === 'microsoft' && !settings.bot.cred.username) {
+		if (settings.bot.cred.auth?.toLowerCase() === 'microsoft' && !settings.bot.cred.username) {
 			warn('When using a Microsoft auth you must specify a password and username', 2);
 			process.exit(1);
 		}
 	}
 
 	// Check if password is defined or null
-	if (settings.bot.cred.auth.toLowerCase() === 'microsoft' && !settings.bot.cred.password) {
+	if (settings.bot.cred.auth?.toLowerCase() === 'microsoft' && !settings.bot.cred.password && settings.bot.cred.password !== null) {
 		settings.bot.cred.password = await prompt('Password :');
 		if (!settings.bot.cred.password) {
 			warn('When using a Microsoft auth you must specify a password and username', 2);
@@ -33,12 +33,12 @@ module.exports = async (settings, chat) => {
 	}
 
 	// Check if server is defined or null
-	if (!settings.bot.cred.server) {
+	if (!settings.bot.cred.server && settings.bot.cred.server !== null) {
 		settings.bot.cred.server = await prompt('Server :');
 	}
 
 	// Check if version is defined or null
-	if (!settings.bot.cred.version) {
+	if (!settings.bot.cred.version && settings.bot.cred.version !== null) {
 		settings.bot.cred.version = await prompt('Version :');
 	}
 
